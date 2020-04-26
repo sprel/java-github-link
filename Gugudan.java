4027599b28739005f19676efb86d4fa54384055d
prel <black7321@naver.com>
2020-04-26 10:32:00 +0900

    v2.0

--- Gugudan.java
+++ Gugudan.java
@@ -8,19 +8,23 @@ public class Gugudan {
 
 		Scanner in = new Scanner(System.in);
 		int dan;
-		System.out.println("v1.5");
+		System.out.println("v2.0");
 		System.out.println("0을 입력하면, 2단부터 9단까지의 구구단을 출력합니다.: ");
 		dan = in.nextInt();
 
-		if (dan == 0) {
-			for (int i = 1; i <= 9; i++) {
-				for (int j = 2; j <= 9; j++) {
-					if (10 <= i*j)
-						System.out.print(j + " X " + i + " = " + j*i + "   ");
-					else
-						System.out.print(j + " X " + i + " = " + j*i + "    ");
+		if (dan < 0) {
+			System.out.println("음수를 입력했습니다. 구구단을 출력할 수 없습니다.");
+		} else {
+			if (dan == 0) {
+				for (int i = 1; i <= 9; i++) {
+					for (int j = 2; j <= 9; j++) {
+						if (10 <= i*j)
+							System.out.print(j + " X " + i + " = " + j*i + "   ");
+						else
+							System.out.print(j + " X " + i + " = " + j*i + "    ");
+					}
+					System.out.println();
 				}
-				System.out.println();
 			}
 		}
 
